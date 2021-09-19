1c0e09a5b1c7f5c1f09745b06bd7eef4ab45083d
Rickshawala <kopardeyuvvraj@gmail.com>
2021-09-19 16:49:35 +0530

    Second Commit

---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,34 @@
+function fish_prompt
+    if test -n "$SSH_TTY"
+        echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
+    end
+
+    echo -n (set_color blue)(prompt_pwd)' '
+
+    set_color -o
+    if fish_is_root_user
+        echo -n (set_color red)'# '
+    end
+    echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+    set_color normal
+end
+
+function ngroke
+	ngrok tcp -region in 25565
+end
+
+alias ls="ls -lagh | lolcat"
+
+alias lsd="lsd -lah"
+
+alias cat="bat"
+
+alias dotfilese="/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
+
+function minecrafte
+	java -jar ~/minecraft-launcher/Tlauncher.jar
+end
+
+function honeygaine
+	~/Documents/honeygain.sh
+end
---
+++ .vimrc
@@ -0,0 +1,9 @@
+syntax on
+set number
+set showcmd
+set cursorline
+filetype indent on
+set wildmenu
+set showmatch
+
+
