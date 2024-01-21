9aadbb3499bb661917cc167ab88584e9d2bfeb1d
Rickshawala <kopardeyuvvraj@gmail.com>
2024-01-21 20:52:51 +0530

    new

---
+++ .config/hypr/autostart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+blueman-applet
+dunst
+hyprpaper
+waybar
+emacs-daemon
+nextcloud
+keepassxc
+alacritty -e vim /home/ykp/Notes/todo.list.md
+asusctl led-mode rainbow
+wl-paste -p -t text --watch clipman store -P --histpath="~/.local/share/clipman-primary.json"
+hyprctl setcursor PUNK-Cyan-Curson 24
+#wl-paste -t text --watch clipman store --no-persist
