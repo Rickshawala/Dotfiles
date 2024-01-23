29a92ff7d8b4b4f669cb0906825cbd8bb1060fe2
Rickshawala <kopardeyuvvraj@gmail.com>
2024-01-23 08:51:50 +0530

    new

--- .config/hypr/autostart.sh
+++ .config/hypr/autostart.sh
@@ -11,4 +11,5 @@ alacritty -e vim /home/ykp/Notes/todo.list.md
 asusctl led-mode rainbow
 wl-paste -p -t text --watch clipman store -P --histpath="~/.local/share/clipman-primary.json"
 hyprctl setcursor PUNK-Cyan-Curson 24
+openrazer-daemon
 #wl-paste -t text --watch clipman store --no-persist
